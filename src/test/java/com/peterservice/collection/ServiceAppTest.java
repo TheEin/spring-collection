b8f24396a630fbc6c647015573c95d5a44250e7d
Aleksandr Kuramshin <Aleksandr.Kuramshin@billing.ru>
2018-04-24 16:09:31 +0300

    Backreference and testing example

--- src/test/java/com/peterservice/collection/ServiceAppTest.java
+++ src/test/java/com/peterservice/collection/ServiceAppTest.java
@@ -1,20 +1,35 @@
 package com.peterservice.collection;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple ServiceApp.
+ * Example test using custom properties
  */
-public class ServiceAppTest
-{
-    /**
-     * Rigorous Test :-)
-     */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceApp.class, properties = {
+        "spring.cloud.zookeeper.enabled=false",
+        "parent1.intProp=555",
+        "parent1.longProp=777" })
+@ActiveProfiles("service")
+public class ServiceAppTest {
+
+    @Autowired
+    ApplicationContext ctx;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        ParentComponent parent = ctx.getBean(ParentComponent.class);
+
+        assertEquals(555, parent.getIntProp());
+
+        assertEquals(777, parent.getLongProp());
     }
 }
